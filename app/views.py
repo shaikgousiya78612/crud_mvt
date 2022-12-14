75b8639a44edc03d9abfc583989d336337e73e87
shaikgousiya78612 <shaikgousiya78612@gmail.com>
2022-12-14 14:29:45 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from app.models import *
+from django.db.models.functions import Length
 # Create your views here.
 def display_topics(request):
     LTO=Topic.objects.all()
+    LTO=Topic.objects.filter(topic_name='kabaddi').order_by('topic_name')
+    LTO=Topic.objects.exclude(topic_name='kabaddi')
+    LTO=Topic.objects.all().order_by('topic_name')
+    LTO=Topic.objects.all().order_by('-topic_name')
+    LTO=Topic.objects.order_by(Length('topic_name'))
+    LTO=Topic.objects.order_by(Length('topic_name').desc())
     d={'LTO':LTO}
     return render(request,'display_topics.html',d)
 
@@ -13,5 +20,13 @@ def display_webpages(request):
 
 def display_AC(request):
     LAO=AccessRecords.objects.all()
+    LAO=AccessRecords.objects.filter(date='2022-12-13')
+    LAO=AccessRecords.objects.exclude(date='2022-12-13')
+    LAO=AccessRecords.objects.order_by('name')
+    LAO=AccessRecords.objects.order_by('-name')
+    LAO=AccessRecords.objects.order_by(Length('name'))
+    LAO=AccessRecords.objects.order_by(Length('name').desc())
+    LAO=AccessRecords.objects.all()[2:3:]
+    LAO=AccessRecords.objects.all()[1:4:]
     d={'LAO':LAO}
     return render(request,'display_AC.html',d)
